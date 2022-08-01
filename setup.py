aa94a1acbacb7376d9c9479191ff19987d1603cb
Tyrone Dingle the 3rd of Dingleton <86061161+roralsorom@users.noreply.github.com>
2022-08-01 18:40:14 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+
+
+setup(name="noCaptchaPy", version='0.0.1', description='Python wrapper for noCaptcha API', author='-_-rormommemm-_-#4779', packages=['noCaptchaPy'], install_requires=['requests', 'selenium', 'undetected_chromedriver', 're', 'json'], zip_safe=False)
